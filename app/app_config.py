0ea4eaf55f69b071fd7162711143f64f1a3c7e9c
Danielpviana <daniel.perez9815@gmail.com>
2024-09-23 02:57:14 -0500

    Bootstrap adjustments

--- app/app_config.py
+++ app/app_config.py
@@ -5,10 +5,8 @@ from app.routes import router
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:my-secret-pw@host.docker.internal/aht-global-db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:my-secret-pw@localhost/aht-global-db' # host.docker.internal
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    #app = db_configure(app)
     
     db.init_app(app)
     
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,9 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from app.db_config import db
 from app.models import Product
 
-router = Blueprint('main', __name__, template_folder='templates')
+router = Blueprint('main', __name__, 
+                   template_folder = 'templates',
+                   static_folder = 'static')
 
 @router.route("/")
 def base():
