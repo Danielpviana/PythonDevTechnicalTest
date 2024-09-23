cab5024e8b9375a9ed4decb751f02ebb415155cf
Danielpviana <daniel.perez9815@gmail.com>
2024-09-23 02:59:35 -0500

    Ajusting docker db route

--- app/app_config.py
+++ app/app_config.py
@@ -5,7 +5,7 @@ from app.routes import router
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:my-secret-pw@localhost/aht-global-db' # host.docker.internal
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:my-secret-pw@host.docker.internal/aht-global-db' # host.docker.internal
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
